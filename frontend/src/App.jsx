d128803851cf7c04f0919a668ccf6d5f1b34ad9c
kah-wtag <kamrul.hasan@welldev.io>
2025-08-31 13:50:57 +0600

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     if (!Message) return;
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/predict", {
+      const response = await fetch("https://welldev-outliers.onrender.com/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ Message }),
